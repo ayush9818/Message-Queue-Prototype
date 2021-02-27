ea7ddd5c0ce2d051ac040af75e60f7f1a8549722
ayush9818 <ayushagarwal1898@gmail.com>
2021-02-27 11:56:59 +0530

    added test cases

--- testing/tests.py
+++ testing/tests.py
@@ -1,122 +1,124 @@
 import sys
 import os
-sys.path.append('..')
-
-
 from imqserver import *
 import time
 import unittest
-from imqclient.client_util import *
-from imqserver.server import *
 import threading
-import multiprocessing
+from imqserver.server import *
 from imqserver.database import *
 import imqserver.db_utils as du
+from imqserver.request import Request
+from imqserver.imqProtocol import imqProtocol
+from imqserver.CommandParser import *
+from imqserver.errors import *
 
-import os
-def func():
-    sys.exit()
 
-counter = 0
 
 class TESTCASES(unittest.TestCase):
 
-    def setUp(self):
-        self.client = ClientUtils()
+    def test_protocol(self):
+        data = "hello"
+        data_protocol = imqProtocol(data)
+        self.assertEqual(data_protocol.data,data)
+        self.assertEqual(data_protocol.format,"json")
+        self.assertEqual(data_protocol.version,"1.0")
 
-    def test_connection(self):
-        '''
-        Check the Connection
-        '''
-        server_response=self.client.CONNECT_SERVER()
-        print("Server response is {}".format(server_response))
-        self.assertEqual(server_response,"Welcome")
+    def test_request(self):
+        data = "hello"
+        data_request = Request(data)
+        self.assertEqual(data_request.data,data)
+        self.assertEqual(data_request.format,"json")
+        self.assertEqual(data_request.version,"1.0")
 
-    def test_admin_connection(self):
-        server_response=self.client.CONNECT_SERVER()
-        # # print("Server response is {}".format(server_response))
+    def test_admin_connect_command(self):
+        command = "imq connect -r admin"
+        parser = CommandParser()
+        response = parser.parse_commands(command)
+        self.assertEqual(response['connect']['role'],'admin')
 
-        commands = ["imq connect -r admin"]
-        server_response = self.client.write(commands,debug=True).split(" ")[-1]
-        self.assertEqual(server_response,"CONNECTED")
+    def test_connect_validity(self):
+        command = "imq connect"
+        parser = CommandParser()
+        response=parser.parse_commands(command)
+        self.assertEqual(response,PROVIDE_ROLE_ERROR)
 
-    # def test_admin(self):
-    #     # client=ClientUtils()
-    #     server_response=self.client.CONNECT_SERVER()
-    #     # # print("Server response is {}".format(server_response))
-    #
-    #     commands = ["imq connect -r admin", "imq register -t sports"]
-    #     server_response = self.client.write(commands,debug=True)
-    #     self.assertEqual(server_response,"Topic Registered")
+    def test_pub_connect_command(self):
+        command = "imq connect -r pub -t sports"
+        parser = CommandParser()
+        response = parser.parse_commands(command)
+        self.assertEqual(response['connect']['role'],'pub')
+        self.assertEqual(response['connect']['topic'],'sports')
 
+    def test_sub_connect_command(self):
+        command = "imq connect -r sub -t sports"
+        parser = CommandParser()
+        response = parser.parse_commands(command)
+        self.assertEqual(response['connect']['role'],'sub')
+        self.assertEqual(response['connect']['topic'],'sports')
 
+    def test_delete_command(self):
+        command = "imq delete"
+        parser=CommandParser()
+        response=parser.parse_commands(command)
+        self.assertEqual(response,DELETE_TOPIC_ERROR)
 
-    def test_pub_connection(self):
-        server_response=self.client.CONNECT_SERVER()
+    def test_topic_existence(self):
+        db_name = "test.db"
+        if os.path.exists(db_name):
+            os.remove(db_name)
+        db = DATABASE("test.db")
+        topic_name = "sports"
+        isExists = du.CheckTopic(db,topic_name)
+        self.assertEqual(isExists,False)
 
-        commands = ["imq connect -r pub -t sports"]
-        server_response = self.client.write(commands,debug=True).split(" ")[-1]
-        self.assertEqual(server_response,"CONNECTED")
+    def test_topic_register(self):
+        db_name = "test.db"
+        if os.path.exists(db_name):
+            os.remove(db_name)
+        db = DATABASE("test.db")
+        topic_name = "sports"
+        du.RegisterTopic(db,topic_name)
+        isExists = du.CheckTopic(db,topic_name)
+        self.assertEqual(isExists,True)
 
-    def test_sub_connection(self):
-        server_response=self.client.CONNECT_SERVER()
+    def test_delete_topic(self):
+        db_name = "test.db"
+        if os.path.exists(db_name):
+            os.remove(db_name)
+        db = DATABASE("test.db")
+        topic_name = "sports"
+        du.RegisterTopic(db,topic_name)
+        du.DeleteTopic(db,topic_name)
+        isExists = du.CheckTopic(db,topic_name)
+        self.assertEqual(isExists,False)
 
-        commands = ["imq connect -r sub -t sports"]
-        server_response = self.client.write(commands,debug=True).split(" ")[-1]
-        self.assertEqual(server_response,"CONNECTED")
+    def test_topic_list(self):
+        db_name = "test.db"
+        if os.path.exists(db_name):
+            os.remove(db_name)
+        db = DATABASE("test.db")
+        topic_name_1 = "sports"
+        du.RegisterTopic(db,topic_name_1)
+        topic_name_2 = "news"
+        du.RegisterTopic(db, topic_name_2)
 
+        fetched_topics = du.fetch_topics(db,type_="list")
+        fetched_topics_1 = fetched_topics[0]
+        fetched_topics_2 = fetched_topics[1]
 
-    def test_publisher(self):
-        #client=ClientUtils()
-        time.sleep(0.1)
-        server_response=self.client.CONNECT_SERVER()
-        commands = ["imq connect -r pub -t sports", "imq publish -m Hello"]
-        server_response = self.client.write(commands,debug=True)
-        self.assertEqual(server_response,"Message Published")
+        self.assertEqual(topic_name_1,fetched_topics_1)
+        self.assertEqual(topic_name_2,fetched_topics_2)
 
-    def test_connect_command(self):
-        time.sleep(0.1)
-        server_response=self.client.CONNECT_SERVER()
-        commands = ["imq connect"]
-        server_response = self.client.write(commands,debug=True)
-        self.assertEqual(server_response,"Provide role. For ex :imq connect -r sub/pub/admin")
-
-    def test_subscriber(self):
-        time.sleep(0.1)
-        server_response=self.client.CONNECT_SERVER()
-        commands = ["imq connect -r sub -t sports", "imq pull"]
-        server_response = self.client.write(commands,debug=True)
-        self.assertEqual(server_response,"Hello")
-
-    def test_show_command(self):
-        time.sleep(0.1)
-        server_response=self.client.CONNECT_SERVER()
-        commands = ["imq show_topics"]
-        server_response = self.client.write(commands,debug=True)
-        self.assertEqual(server_response,"sports\n")
-
-    def test_func(self):
-        self.assertRaises(SystemExit, func)
-
-
-
-
-
-def run_server(db_name):
-    new_server = Server(db_name)
-    new_server.run_server(debug=False)
-
-
-
-
-if __name__=='__main__':
-    db_name="client_server.db"
-
-    server_thread=threading.Thread(target=run_server,args=(db_name,))
-    server_thread.start()
-    time.sleep(0.1)
-
-
-    unittest.main()
-    server_thread.join()
-    sys.exit(0)
+    def test_new_client(self):
+        db_name = "test.db"
+        if os.path.exists(db_name):
+            os.remove(db_name)
+        db = DATABASE("test.db")
+        topic_name = "sports"
+        du.RegisterTopic(db,topic_name)
+        address = "172.1.0.1_1234"
+        role = "pub"
+        status = du.HandleNewClient(db,address,topic_name,role)
+        self.assertEqual(status,False)
+        status = du.HandleNewClient(db,address,topic_name,role)
+        self.assertEqual(status,True)
