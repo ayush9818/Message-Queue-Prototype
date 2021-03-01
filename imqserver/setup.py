2bfe22e4b7dc714cf4230850151dc69a3189d38b
ayush9818 <ayushagarwal1898@gmail.com>
2021-03-01 13:33:41 +0530

    final commit

--- imqserver/setup.py
+++ imqserver/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup (name="imqserver",
-        version="1.1",
+        version="1.0",
         description="Server package",
-        author="Xyz",
+        author="Ayush Agarwal",
         packages=find_packages(), install_requires = ['jsonpickle'])
